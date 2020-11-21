3e715c08a32d106a87ba66c8bdb9efbb34e487cd
Quanmuito <49555971+Quanmuito@users.noreply.github.com>
2020-11-21 17:03:43 +0200

    Updates

--- src/App.js
+++ src/App.js
@@ -15,8 +15,8 @@ export default function App() {
             <Route exact path="/">
               <Home />
             </Route>
-            <Route exact path="/:id">
-              <Profile/>
+            <Route path="/:id">
+              <Profile />
             </Route>
           </Switch>
         </Container>
